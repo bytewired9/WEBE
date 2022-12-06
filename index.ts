1a3a2f8df950cbe3619322e08d440e335e89d449
ForgedCore8 <77468737+ForgedCore8@users.noreply.github.com>
2022-12-06 10:33:25 -0600

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { events } from "bdsx/event";
+
+
+console.log('[ExamplePlugin] allocated');
+// before BDS launching
+
+events.serverOpen.on(()=>{
+    console.log('[ExamplePlugin] launched');
+    // after BDS launched
+});
+
+events.serverClose.on(()=>{
+    console.log('[ExamplePlugin] closed');
+    // after BDS closed
+});
+
+
