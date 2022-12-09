73ee151640cdb6cebb9402c683631eecb0738884
Grape <sacrigrape@gmail.com>
2022-12-09 00:58:35 -0600

    Fixed command

--- Commands/wand.ts
+++ Commands/wand.ts
@@ -5,12 +5,23 @@ import { events } from "bdsx/event";
 
 events.serverOpen.on(() => {
     command
-    .register('wwand', 'Give WEBE wand', CommandPermissionLevel.Operator)
-    .overload((param, origin, output) => {
-        const player = origin.getEntity();
-        if (!player?.isPlayer()) {
-            output.error('Command needs to be ran by a player!');
-            return;
-        }
-    }, {});
+        .register('wwand', 'Give WEBE wand', CommandPermissionLevel.Operator)
+        .overload((param, origin, output) => {
+            const player = origin.getEntity();
+            if (!player?.isPlayer()) {
+                output.error('Command needs to be ran by a player!');
+                return;
+            }
+
+            const wandItem = ItemStack.constructWith('webe:wand', 1);
+            if (wandItem.getName() === 'minecraft:air') {
+                output.error('Cant get wand item! Are you sure you have the RP and BP installed?');
+                return;
+            }
+
+            player.addItem(wandItem);
+            player.sendInventory();
+
+            output.success('§o§5Successfully Given Wand')
+        }, {});
 });
